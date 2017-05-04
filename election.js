969558a20cf83c014ee0cff27ac7e8f89fc3bfd9
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-05-04 00:49:23 -0400

    Part 1 Bikini Bottom

--- election.js
+++ election.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+  }).done(function(responseData) {
+    for (var i = 0; i < responseData.length; i++) {
+      candidate = data.list[i]
+      name = candidate.name
+      votes = candidate.votes
+      info = name + ' has ' + votes + ' vote.'
+      $('<li>').html(info).appendTo('#list');
+    }
+    console.log('done')
+  });
   // Imagination!
-
 });
